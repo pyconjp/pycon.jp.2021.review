cd579f887abcd8d4696a6785c37ce5423c1af77e
ftnext <takuyafjp+develop@gmail.com>
2021-06-13 01:14:30 +0900

    [feat] List proposals for logged-in users (close #3)

--- reviewsite/review/views.py
+++ reviewsite/review/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Proposal
+
+
+@login_required
+def list_proposals(request):
+    proposals = Proposal.objects.order_by("sessionize_id")
+    context = {"page_obj": proposals, "proposals_count": len(proposals)}
+    return render(request, "review/list_proposals.html", context)
