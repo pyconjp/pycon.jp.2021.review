8dfb7cad5e70e85b8c94d2be4567b2b7cb6bfc56
ftnext <takuyafjp+develop@gmail.com>
2021-07-11 21:53:15 +0900

    [feat] Add templatetag to map count to color

--- reviewsite/review/templatetags/custom_filter.py
+++ reviewsite/review/templatetags/custom_filter.py
@@ -8,3 +8,15 @@ def query_replace(request, field, value):
     params_dict = request.GET.copy()
     params_dict[field] = value
     return params_dict.urlencode()
+
+
+@register.simple_tag
+def color_by_count(count: int) -> str:
+    if count >= 4:
+        return "success"
+    elif count == 3:
+        return "primary"
+    elif count == 2:
+        return "warning"
+    else:
+        return "danger"
